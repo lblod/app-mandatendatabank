4e867a1b1190f76ef273b3715532e6b8108cf85d
Erika Pauwels <erika.pauwels@gmail.com>
2019-12-19 16:09:22 +0100

    Bump mu-authorization

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -9,22 +9,6 @@ alias Acl.GroupSpec, as: GroupSpec
 alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
 
 defmodule Acl.UserGroups.Config do
-  defp access_by_role( group_string ) do
-    %AccessByQuery{
-      vars: ["session_group","session_role"],
-      query: sparql_query_for_access_role( group_string ) }
-  end
-
-  defp sparql_query_for_access_role( group_string ) do
-    "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-    SELECT ?session_group ?session_role WHERE {
-      <SESSION_ID> ext:sessionGroup/mu:uuid ?session_group;
-                   ext:sessionRole ?session_role.
-      FILTER( ?session_role = \"#{group_string}\" )
-    }"
-  end
-
   def user_groups do
     # These elements are walked from top to bottom.  Each of them may
     # alter the quads to which the current query applies.  Quads are
@@ -37,7 +21,7 @@ defmodule Acl.UserGroups.Config do
       %GroupSpec{
         name: "public",
         useage: [:read],
-        access: %AlwaysAccessible{}, # TODO: Should be only for logged in users
+        access: %AlwaysAccessible{},
         graphs: [ %GraphSpec{
                     graph: "http://mu.semte.ch/application",
                     constraint: %ResourceConstraint{
@@ -62,15 +46,7 @@ defmodule Acl.UserGroups.Config do
                         "http://schema.org/PostalAddress",
                         "http://www.w3.org/ns/org#Role",
                         "http://www.w3.org/ns/org#Site"
-                    ] } } ] },
-
-      # // CLEANUP
-      #
-      %GraphCleanup{
-        originating_graph: "http://mu.semte.ch/application",
-        useage: [:write],
-        name: "clean"
-      }
+                    ] } } ] }
     ]
   end
 end
