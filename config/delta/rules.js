51b467a6cd253ceb13708691b7c9231e8ac2974f
felix <ruiz.felixruiz@gmail.com>
2021-08-17 17:30:29 +0200

    update the rules for report-generation

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -14,12 +14,37 @@ export default [
       optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/consumer/initialSync" ]
     }
   },
-  {
+ {
     match: {
-      // anything
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://redpencil.data.gift/vocabularies/deltas/Error'
+      }
     },
     callback: {
-      url: 'http://mandatarissen-producer/delta',
+      url: 'http://delta-producer-report-generator/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/consumer/initialSync" ]
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      }
+    },
+    callback: {
+      url: 'http://delta-producer-report-generator/delta',
       method: 'POST'
     },
     options: {
